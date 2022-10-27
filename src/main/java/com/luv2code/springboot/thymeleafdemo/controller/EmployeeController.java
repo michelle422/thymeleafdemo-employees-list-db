a0778edbbc28d5d3922b693ac9835de43d2f8926
michelle422 <chelo.pablo@gmail.com>
2022-10-28 00:11:20 +0200

    Initial commit

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,38 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	private EmployeeService employeeService;
+
+	public EmployeeController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
+	}
+
+	// add mapping for "/list"
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+
+		// get employees from db
+		List<Employee> theEmployees = employeeService.findAll();
+		
+		// add to the Spring model
+		theModel.addAttribute("employees", theEmployees);
+		
+		return "list-employees";
+	}
+}
