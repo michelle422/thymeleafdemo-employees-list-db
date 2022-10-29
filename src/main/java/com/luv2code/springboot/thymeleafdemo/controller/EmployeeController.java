ceb9df65b63ee70433f42ed8eec58315da957746
michelle422 <chelo.pablo@gmail.com>
2022-10-29 11:57:23 +0200

    Added Add button

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,17 +1,15 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.PostConstruct;
-
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import java.util.List;
 
 @Controller
 @RequestMapping("/employees")
@@ -20,6 +18,7 @@ public class EmployeeController {
 	private EmployeeService employeeService;
 
 	public EmployeeController(EmployeeService theEmployeeService) {
+
 		employeeService = theEmployeeService;
 	}
 
@@ -33,6 +32,28 @@ public class EmployeeController {
 		// add to the Spring model
 		theModel.addAttribute("employees", theEmployees);
 		
-		return "list-employees";
+		return "employees/list-employees";
 	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		// create model attribute to bind form data
+		Employee theEmployee = new Employee();
+		
+		theModel.addAttribute("employee", theEmployee);
+		
+		return "employees/employee-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		
+		// save the employee
+		employeeService.save(theEmployee);
+		
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/employees/list";
+	}
+
 }
